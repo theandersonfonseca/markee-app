652da8041c7011b9ec1a95eb7d303b83e607bae9
Anderson <andersonfonseca1999@hotmail.com>
2021-09-20 23:25:50 -0300

    Add Sidebar and Content to app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,20 @@
+import { Content } from 'components/content'
+import { Sidebar } from 'components/sidebar'
+
+import styled from 'styled-components/macro'
+
 function App () {
-  return <div>app</div>
+  return (
+    <Wrapper>
+      <Sidebar />
+      <Content />
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  min-height: 100vh;
+`
 
 export { App }
